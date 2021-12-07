f16e46a7f502a776a60377fe8831f7588474d61c
mb1te <45900239+mb1te@users.noreply.github.com>
2021-12-07 20:54:50 +0300

    Update JavaEndOfFile.java

--- Task1 - Java Tasks From HackerRank/JavaEndOfFile.java
+++ Task1 - Java Tasks From HackerRank/JavaEndOfFile.java
@@ -5,7 +5,6 @@ import java.math.*;
 import java.util.regex.*;
 
 public class JavaEndOfFile {
-
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         
