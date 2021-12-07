2cc7ef71c823693b97ec6c29ab1e57b4b746722f
mb1te <45900239+mb1te@users.noreply.github.com>
2021-12-07 21:01:19 +0300

    Update JavaStdinAndStdoutI.java

--- Task1 - Java Tasks From HackerRank/JavaStdinAndStdoutI.java
+++ Task1 - Java Tasks From HackerRank/JavaStdinAndStdoutI.java
@@ -1,7 +1,6 @@
 import java.util.*;
 
 public class JavaStdinAndStdoutI {
-
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int a = scan.nextInt();
